d5d483722c531afe27a4a9aa2feba931ae4024b7
deser <yongk202@163.com>
2020-07-04 03:39:56 +0800

    add cmp extra_strcmp extra_itoa extra_strcpy

--- extra.h
+++ extra.h
@@ -1,38 +1,37 @@
 #ifndef _EXTRA_H_
 #define _EXTRA_H_
 
-#define MAXCAPACITY 0x7fffffff
-
-
-typedef struct path_node
-{
-    int v;//结点编号
-    int path_end;//路径字符数组的末尾指针
-}Pnode;
+#define MAXCAPACITY 0xffffff
 
 //栈
 typedef struct search_stack {
     int top;
-    Pnode data[MAXCAPACITY];
+    int data[MAXCAPACITY];
 } S_Stack;
 void initStack(S_Stack** S);
-void push(Pnode d, S_Stack* S);
-Pnode pop(S_Stack* S);
+void push(int d, S_Stack* S);
+int pop(S_Stack* S);
 void deleteStack(S_Stack* S);
+int isStackEmpty(S_Stack* S);
+int isStackFull(S_Stack* S);
 
 //队列
 typedef struct search_queue {
     int front, rear;
-    Pnode data[MAXCAPACITY];
+    int data[MAXCAPACITY];
 } S_Queue;
 void initQueue(S_Queue** Q);
-void enQueue(Pnode d, S_Queue* S);
-Pnode deQueue(S_Queue* S);
+void enQueue(int d, S_Queue* S);
+int deQueue(S_Queue* S);
 void deleteQueue(S_Queue* Q);
+int isQueueEmpty(S_Queue* Q);
+int isQueueFull(S_Queue* Q);
 
-int strcmp(char* a, char* b);               //字符串比较
-int itoa(int x, char* str);                 //把一个整数写入字符串中，返回写入的长度
-int strcpy(char* dest, char* source);       //字符串复制，返回复制的长度
+int extra_strcmp(char* a, char* b); //字符串比较
+int extra_itoa(int x, char* str); //把一个整数写入字符串中，返回写入的长度
+int extra_strcpy(char* dest, char* source); //字符串复制，返回复制的长度
 
+//以arr[0]、arr[1]分别为第一、第二关键词比较
+int cmp(const void* a, const void* b);
 
 #endif
